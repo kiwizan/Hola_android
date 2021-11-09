a8768ac68936956eb252c5fd937128e4a3ef93d6
William Gutierrez <whgutierrezr@gmail.com>
2021-11-09 06:07:21 -0500

    second

--- app/src/main/java/com/whgutierezr/holaandroid/MainActivity.kt
+++ app/src/main/java/com/whgutierezr/holaandroid/MainActivity.kt
@@ -3,9 +3,11 @@ package com.whgutierezr.holaandroid
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+class MainActivity : AppCompatActivity() { // el extend o herencia se hace solo con los dos puntos :
+    override fun onCreate(savedInstanceState: Bundle?) {    //sobre escribiendo la clase
+                                                            // fun hace refeerencia a que es una funcion
+        super.onCreate(savedInstanceState)                  // llama al atributo metodo de la clase padre
         setContentView(R.layout.activity_main)
     }
+
 }
